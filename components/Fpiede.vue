f0de875597401310114b5e4eb1255c572a931776
Andrea <andrea3aprile99@gmail.com>
2022-01-25 11:50:24 +0100

    t

---
+++ components/Fpiede.vue
@@ -0,0 +1,65 @@
+<template>
+ 
+    <v-footer
+      v-bind="localAttrs"
+      :padless="padless"
+    >
+      <v-card
+        flat
+        tile
+        width="100%"
+        class="  lighten-1 text-center"
+      >
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4"
+            icon
+          >
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Luxury Home Roseto degli Abruzzi</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+</template>
+<script>
+  export default {
+    data: () => ({
+      icons: [
+        'mdi-home',
+        'mdi-email',
+        'mdi-whatsapp',
+        'mdi-instagram',
+      ],
+      items: [
+        'default',
+        'absolute',
+        'fixed',
+      ],
+      padless: false,
+      variant: 'default',
+    }),
+    computed: {
+      localAttrs () {
+        const attrs = {}
+
+        if (this.variant === 'default') {
+          attrs.absolute = false
+          attrs.fixed = false
+        } else {
+          attrs[this.variant] = true
+        }
+        return attrs
+      },
+    },
+  }
+</script>
---
+++ components/TheNavbar.vue
@@ -0,0 +1,37 @@
+<template>
+<div>
+    <ul>
+        <li><NuxtLink to="/dove-siamo" prefetchLinks><p id="linkcactivelass">Dove siamo</p> </NuxtLink></li>
+        <li><NuxtLink to="/interni-esterni" prefetchLinks><p id="linkcactivelass">Interno ed Esterno</p></NuxtLink></li>
+        <li><NuxtLink to="/ordina-ora" prefetchLinks><p id="linkcactivelass"> Prenota</p></NuxtLink></li>
+        <li><NuxtLink to="/contatti" prefetchLinks><p id="linkcactivelass">Contattaci</p></NuxtLink></li> 
+    </ul>
+
+
+</div>
+</template>
+
+
+<style >
+#linkcactivelass{
+ color:#fff;
+   list-style: none;
+   text-decoration: none;
+}
+   ul li{
+ color:#fff;
+   list-style: none;
+   text-decoration: none;
+}
+
+.nuxt-link-active {
+  font-weight: bold;
+ 
+  
+}
+/* exact link will show the primary color for only the exact matching link */
+.nuxt-link-exact-active {
+  color:red;
+}
+
+</style>
