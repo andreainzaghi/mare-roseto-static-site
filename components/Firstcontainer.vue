7facec1d273e2129cc23438c8673dbcaabacff40
Andrea <andrea3aprile99@gmail.com>
2022-01-25 11:42:30 +0100

    h

---
+++ components/Firstcontainer.vue
@@ -0,0 +1,108 @@
+<template>
+<div>
+     <div  class="container">
+            <video  style="z-index:1;" id="background1-video1"  src="@/image/video.mp4" loop autoplay muted ></video>
+             <div class="scroll-down"><i class="fas fa-chevron-circle-down" ></i></div>
+           <div  style="z-index:1;" class="basso-video">
+              
+                 
+                            <div>32m <br>
+                         <b> dalla Spiaggia</b>  </div>
+                       
+                            <div>75m <br>
+                         <b>dal Mare</b>   </div>
+                       
+                            <div>75m <sup>2</sup> <br>
+                         <b> Superficie</b>  </div>
+                         <div>Roseto degli Abruzzi<br><b>Viale Makarska 24</b></div>
+                       
+                            <div id="btn1"><NuxtLink to="/ordina-ora" prefetchLinks><span id="linkcactivelass">PRENOTA</span></NuxtLink></div>
+                
+           </div>
+           
+        </div>
+         <div style="z-index:2;">
+            <Carosel/>
+         <video id="background-video" style="z-index:1;"  src="@/image/Roseto.mp4" loop autoplay muted ></video>
+         <Carosel/>
+         <Carosel/>
+         </div>
+      
+          </div>
+</template>
+
+<style scoped>
+.fa-chevron-circle-down{
+   animation: identifier 1s linear 1s infinite alternate;
+}
+   
+@keyframes identifier {
+  0% {color:rgb(255, 255, 255);  }
+  50% { color:rgba(255, 255, 255, 0.205);  transform: scale(1.06);}
+  100% { color:rgb(255, 255, 255);  }
+}
+#btn1{
+    background-color: transparent;
+    border:none;
+   
+    border-radius: 50px;
+   
+    padding:8px 24px;
+   
+    color:#fff;
+}
+#btn1:hover{
+    background-color: transparent;
+    border:none;
+  
+    border-radius: 50px;
+    border:2px solid #fff;
+    padding:8px 24px;
+   
+    color:#fff;
+}
+.basso-video div button{
+    background-color: transparent;
+    border:none;
+    border-radius: 50px;
+    border:2px solid #fff;
+    padding:8px 24px;
+    color:#fff;
+}
+.basso-video {
+    width:90%;
+    height:150px;
+    margin:auto;
+z-index: 1;
+    position:fixed;
+   color:#fff;
+    right:0;
+    bottom:0;
+    left:0;
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    font-size: 24px;
+}
+.basso-video div b{
+    font-size: 15px;
+}
+.scroll-down{
+  z-index:1;font-size:60px;position:fixed;top:calc(70% - 30px);left:calc(50% - 30px);
+}
+@media only screen and (max-width: 920px) {
+   .scroll-down{
+  z-index:1;font-size:60px;position:fixed;top:calc(30% - 30px);left:calc(50% - 30px);
+}
+  .basso-video{
+     display:block;
+     position:relative;
+     text-align: center;
+  }
+  .container{
+     display:flex;
+     align-items:center;
+     justify-content: center;
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,66 +1,32 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+   
+   
     <v-app-bar
       :clipped-left="clipped"
       fixed
       app
+     
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+     <nav>
+      
+       
+            <span id="linknav">  <NuxtLink to="/contatti" prefetchLinks> <span id="linkcactivelass">Contattaci</span> </NuxtLink></span>
+           <NuxtLink to="/" prefetchLinks style="display:flex;align-items:center;justify-content:center;"><span id="linkcactivelass">APPARTAMENTOLUXURYVACANZEMARE</span></NuxtLink>
+            <span id="linknav"> <NuxtLink to="/ordina-ora" prefetchLinks><span id="linkcactivelass">Prenota</span></NuxtLink></span>
+        </nav>
       <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
+      style="color:#fff;"
         icon
         @click.stop="rightDrawer = !rightDrawer"
       >
-        <v-icon>mdi-menu</v-icon>
+        <v-icon  >mdi-menu</v-icon>
       </v-btn>
     </v-app-bar>
-    <v-main>
-      <v-container>
-        <Nuxt />
-      </v-container>
+    <v-main style="padding:15px;margin-top:60px;">
+     
+           <Nuxt />
+     
     </v-main>
     <v-navigation-drawer
       v-model="rightDrawer"
@@ -75,16 +41,19 @@
               mdi-repeat
             </v-icon>
           </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+          <v-list-item-title> 
+           
+            </v-list-item-title>
+         
         </v-list-item>
+         <div style="height:300px;width:100%;">
+           <TheNavbar/>
+         </div>
       </v-list>
     </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+   <div>
+     <Fpiede/>
+   </div>
   </v-app>
 </template>
 
@@ -96,22 +65,300 @@ export default {
       drawer: false,
       fixed: false,
       items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+        
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Luxury Estate Roseto'
     }
   }
 }
 </script>
+ <style >
+
+
+
+
+#background{
+    
+    height:100vh;
+}
+.interni-casa{
+    width:100%;
+  margin:auto;
+    padding:40px;
+}
+.sezione-vista{
+   width:100%;
+   display: flex;
+   align-items: center;
+}
+.sezione-vista img{
+    width:50%;
+}
+.sezione-vista p{
+   padding: 0 40px;
+   font-size: 20px;
+}
+.sez-video12{
+    position:absolute;
+    width:240px;
+    height: 70px;
+    z-index: 156;
+    right:0;
+    border-top-left-radius: 10px;
+    padding:10px;
+    bottom:0;
+    color:#fff;
+    background-color: black;
+}
+.carosello12{
+    width:100%;
+    height:100vh;
+    background-color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.titolosezione2{
+    text-align: center;
+    color: rgb(255, 255, 255);
+    text-shadow: 1px 1px 1px black;
+}
+.sezione223{
+  /* border:1px solid red; */
+    height:100vh;
+  /* display: flex;
+  justify-content: center;
+  align-items: center; */
+    width:100%;
+    background-color: black;
+    /* background-image:url('img/vistaspiaggia3.jpeg') ; */
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+ 
+}
+.sezione2 video{
+    margin:auto;
+}
+.title1 b{
+    font-size: 15px;
+   
+}
+.title1{
+    /* font-family: 'Times New Roman', Times, serif; */
+    font-weight: 100;
+    text-shadow: 1px 1px 0px rgba(55, 66, 111, 0.172);
+    text-align: center;
+    color:#fff3c968;
+    font-size: 40px;
+    margin-top:200px;
+}
+
+
+*{
+    margin:0;
+    padding:0;
+    font-family: 'Outfit', sans-serif;
+    box-sizing: border-box;
+}
+.container{
+    width:100%;
+    height: 100vh;
+  
+   
+}
+nav{
+    display:flex;
+   
+    justify-content: center;
+    align-items: center;
+    height:100%;
+    /* position:fixed;
+    top:0;
+    left:0;
+    right:0; */
+    margin:auto;
+    width:90%;
+    padding:20px;
+}
+nav img{
+    width:60%;
+  
+    
+}
+nav span{
+    padding:0 60px;
+    color:#fff;
+    text-shadow: 1px 1px 1px rgb(0, 0, 0);
+}
+video{
+    height: calc(100vh - 60px);
+    width:100%;
+    
+}
+
+#background-video {
+width: 100%;
+height: 100vh;
+object-fit: cover;
+position: relative;
+left: 0;
+right: 0;
+top: 0;
+bottom: 0;
+z-index: -1;
+}
+#background1-video1 {
+width: 100%;
+height: 100vh;
+object-fit: cover;
+position: fixed;
+left: 0;
+right: 0;
+top: 0;
+bottom: 0;
+z-index: -1;
+}
+
+/* carosello */
+.page-container {
+height: 100vh;
+}
+
+.title {
+display: flex;
+align-items: center;
+justify-content: center;
+height: 30vh;
+}
+
+#title-p-1 {
+text-align: center;
+font-size: 40px;
+padding: 60px 10px 0 0;
+text-shadow: 3px 4px 1px rgba(0, 0, 0, 0.2);
+
+}
+
+#nola {
+font-size: 90px;
+font-family: 'Arizonia', cursive;
+transform: rotate(-10deg);
+text-shadow: 3px 4px 1px rgba(0, 0, 0, 0.2);
+}
+
+.carousel-wrapper {
+display: flex;
+justify-content: center;
+align-items: center;
+
+width:99%;
+height:75%;
+}
+
+.carousel {
+height:100%;
+display: flex;
+
+align-items: center;
+width: 100%;
+
+position: relative;
+}
+
+.carousel__slide {
+width: 100%;
+height: 100%;
+border-radius: 2px;
+overflow: hidden;
+box-shadow: 10px 10px 10px 1px rgba(0, 0, 0, .2);
+}
+
+.carousel__slide img {
+width: 100%;
+height: 100%;
+object-fit: cover;
+object-position: center;
+}
+
+.carousel__prev{
+font-size: 20px;
+padding: 5px 10px;
+border:2px solid #fff;
+cursor: pointer;
+margin:0 20px;
+color: rgb(255, 255, 255);
+position:absolute;
+left:0;
+background-color: rgba(0, 0, 0, 0.6);
+}
+.carousel__next {
+font-size: 20px;
+background-color: rgba(0, 0, 0, 0.6);
+padding: 5px 10px;
+border:2px solid #fff;
+cursor: pointer;
+margin:0 20px;
+color: rgb(255, 255, 255);
+position:absolute;
+right:0;
+/* color: #D4AF37; */
+}
+
+.carousel__bullets {
+position: absolute;
+top: calc(100% + 20px);
+left: 50%;
+transform: translateX(-50%);
+padding: 10px ;
+background: rgba(0, 0, 0, 0.4);
+border-radius: 30px;
+display: flex;
+align-items: center;
+z-index: 333;
+}
+
+.carousel__bullets i {
+font-size: 10px;
+margin: 0 5px;
+
+color: #fff;
+cursor: pointer;
+}
+
+.carousel__bullets .active {
+color: #ffd139;
+}
+/* carosello */
+/* scroll */
+
+/* width */
+::-webkit-scrollbar {
+width: 0px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+box-shadow: inset 0 0 0px grey; 
+border-radius: 0px;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+background: red; 
+border-radius: 0px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+background: #b30000; 
+}
+@media only screen and (max-width: 920px) {
+  #linknav{
+    display:none;
+  }
+}
+ </style>
